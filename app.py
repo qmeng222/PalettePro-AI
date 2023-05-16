92973c0ba9447a4c82c74439797dfac91b7cca8f
Qingying Meng <qmeng222@gmail.com>
2023-05-15 21:12:20 -0700

    integrated OpenAI with server

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
+import openai
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__, template_folder="templates")
 
-
-@app.route("/guinea-pig")
-def gp():
-    return "Wheek! Wheek!🥬"
+# @app.route("/palette", methods=["POST"])
+# def prompt_to_palette():
+#     # OpenAI completion call:
+#     # Return list of colors:
 
 
 @app.route("/")
 def index():
-    return "Hello from Flask👋"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Give me a funny word: ",
+        max_tokens=200,
+    )
+    return response["choices"][0]["text"]
+    # return render_template("index.html")
 
 
 if __name__ == "__main__":
